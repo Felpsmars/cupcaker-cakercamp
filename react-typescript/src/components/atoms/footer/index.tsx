ce59ce0cd1834442a8957fb102fec3cb803339ec
felpsmars <mfelipeluiz99@gmail.com>
2022-04-07 11:35:37 -0300

    CUP-13: Creating components header and footer

---
+++ react-typescript/src/components/atoms/footer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './styles.module.scss'
+
+const Footer = (): JSX.Element => {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <p>© 2022 Caker Camp - Game Vicio Redesign - Cheesecake Labs</p>
+      </div>
+    </footer>
+  )
+}
+
+export { Footer }
