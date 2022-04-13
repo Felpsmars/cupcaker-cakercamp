9f1b41343ecda97901ba9e020e5258805bb08d32
felpsmars <mfelipeluiz99@gmail.com>
2022-04-13 13:33:47 -0300

    CUP-9_Refac: refactoring stories component to render more cards

--- react-typescript/src/app/core/pages/home/index.tsx
+++ react-typescript/src/app/core/pages/home/index.tsx
@@ -3,12 +3,20 @@ import React from 'react'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
+import { Stories } from 'components/atoms'
+
+import stories from '../../../../data/stories.json'
+
 moment.locale('pt-br')
 
 /* import styles from './styles.module.scss'*/
 
 const Home = (): JSX.Element => {
-  return <main>I AM MAIN</main>
+  return (
+    <main>
+      <Stories stories={stories} />
+    </main>
+  )
 }
 
 export default Home
