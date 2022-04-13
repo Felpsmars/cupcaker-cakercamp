83a31212817491519434408803304872265f3d00
felpsmars <mfelipeluiz99@gmail.com>
2022-04-13 09:50:35 -0300

    DEV:Fix

--- react-typescript/src/app/core/pages/home/index.tsx
+++ react-typescript/src/app/core/pages/home/index.tsx
@@ -3,27 +3,12 @@ import React from 'react'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 
-import { HomeNews } from 'components/molecules'
-
 moment.locale('pt-br')
 
 /* import styles from './styles.module.scss'*/
 
 const Home = (): JSX.Element => {
-  return (
-    <main>
-      <HomeNews
-        author="Billy Butcher,"
-        title="Title: Lorem Ipsum Lorem Ips"
-        description="Description: Lorem Ipsum Lorem "
-        comments="44"
-        label="PS4"
-        stylesTag="PS4"
-        time={'2022-04-06T09:00:46.272Z'}
-        srcIMG="https://static.gamevicio.com/imagens_up/big/71/sonic-the-hedgehog-2-gerou-us-25-5-milhoes-em-seu-final-de-semana-internacional-070121.jpg"
-      />
-    </main>
-  )
+  return <main>I AM MAIN</main>
 }
 
 export default Home
